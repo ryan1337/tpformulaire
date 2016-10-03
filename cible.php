633aab87132cfebb6f05f50755a1bcb44ccd39ef
ryan1337 <ryanaouiche@gmail.com>
2016-10-03 03:59:54 +0200

    tp formulaire avec envoie de fichier

---
+++ cible.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <title>Formulaire post</title>
+    </head>
+    <body>
+        
+        <?php 
+
+        if (isset($_POST['prenom']) && $_POST['prenom'] == true) {
+            echo 'Bonjour ' . htmlspecialchars($_POST['prenom']);
+        }else {
+            echo ' veuillez entrer votre prenom';
+        }
+        if (isset($_POST['nom']) && $_POST['nom'] == true ) {
+            echo ' ' . htmlspecialchars($_POST['nom']);
+        }else {
+            echo  ' veuillez entrer votre nom';
+        }
+        if (isset($_POST['age']) && $_POST['age'] > 0) {
+            $_POST['age'] = (int) $_POST['age'];
+            echo ' vous avez ' . htmlspecialchars($_POST['age'] . ' ans');
+        }else {
+            echo ' veuillez entrer votre age';
+        }
+        if ($_POST['sexe'] == 'homme') {
+            echo ' vous etes un homme';  
+        }
+        elseif ($_POST['sexe'] == 'femme'){
+            echo ' vous etes une femme';
+        }
+        else {
+            echo ' veuillez cocher une case';
+        }
+         if (isset($_POST['ville']) && $_POST['ville'] == true) {
+             echo ' vous habitez ' .htmlspecialchars($_POST['ville']);
+         }
+         else {
+             echo ' veuillez remplire la case';
+         }
+        if ($_POST['loisirs'] == 'boxe') {
+            echo ' vous pratiqué la boxe';
+        }
+        elseif ($_POST['loisirs'] == 'musculation') {
+            echo ' vous pratiqué la musculation';
+        }
+        elseif ($_POST['loisirs'] == 'basket') {
+             echo ' vous pratiqué le basket-ball';
+         }
+        elseif ($_POST['loisirs'] == 'cinema') {
+            echo ' vous etes fan de cinema';
+        }
+       
+        else  {
+            echo ' veuillez cocher une case';
+        }
+
+        if (isset($_FILES['fichier']) && $_FILES['fichier']['error'] == 0) {
+         if ($_FILES['fichier']['size'] <= 100000) {
+             $infosfichier = pathinfo($_FILES['fichier']['names']);
+             $extension_upload = $infofichier['extension'];
+             $extension_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+             if (in_array($extension_upload, $extension_autorisees)) {
+                 move_uploaded_file($_FILES['fichier']['tmp_name'], 'uploads/' . basename($_FILES['fichier']['name']));
+              echo 'L\' envoi a bien été effectué !';  
+             }
+         }   
+        }
+
+        ?>
+    </body>
+</html>
---
+++ formulaire.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <title>Formulaire post</title>
+    </head>
+    <body>
+       
+        
+            <form action="cible.php" method="post" >
+             <ul>
+                  
+              <li> <label for=""> Prenom : <input type="text" name="prenom"/> </label></li> <br />
+              <li> <label for="">Nom : <input type="text" name="nom"/> </label></li> <br />
+              <li> <label for="">Age: <input type="text" name="age"/> </label></li> <br />
+              <li> <label for="sexe">sexe : <input type="radio" name="sexe" value="homme" id="homme"/> 
+                    <label for="homme">Homme</label>  
+                    <input type="radio" name="sexe" value="femme" id="femme" /> <label for="femme">femme</label> </label></li> <br />
+              <li> <label for="">ville : <input type="text" name="ville"/> </label></li>  <br />
+              <li> <label for="">loisirs:</label></li> <br >
+               <li> <label for=""> Boxe </label> <input type="checkbox" name="loisirs" value="boxe"/>  </li>
+                <li> <label for=""> Musculation</label> <input type="checkbox" name="loisirs" id="" value="musculation" />   </li>
+                <li> <label for="">Basket</label> <input type="checkbox" name="loisirs" id="" value="basket" /></li>
+               <li> <label for=""> Cinéma</label> <input type="checkbox" name="loisirs" id="" value="cinema" />  </li> 
+               <li><input type="submit" name="cible.php" id="" value="valider" /></li>  <br>
+
+            </form>
+            <form action="cible_fichier.php" method="post" enctype="multipart/form-data">
+                
+            </form>
+               <li> <input type="file" name="fichier" id="" value="" /></li><br>
+                <li><input type="submit" id="" value="Envoyer le fichier" name="cible_fichier.php" /></li><br >
+             </ul>
+            </form>
+
+
+
+    </body>
+</html>
